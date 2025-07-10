9cb81fc2aab63a24ca617171b073df41ea865509
OdinDev <odinDevs@proton.me>
2025-07-10 22:08:49 +0100

    initial file change, add asset, update html, comment js logic

---
+++ main.js
@@ -0,0 +1,13 @@
+// import date from date
+//
+
+
+// greet user function
+// `get hour and assign it to a variable
+// `if hour is < 12  AND  < 18then change the greeting-text to Good Morning, John`
+// `if hour is > 12  AND < 18 then change greeting-text to Good Afternoon, John`
+// `if hour is > 12  AND > 18 then change greeting-text to Good Evening, John`
+
+// function greetUser(){
+//    
+// }
