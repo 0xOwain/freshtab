435b3be7b73682367638afa851eb8329e3a8690f
OdinDev <odinDevs@proton.me>
2025-07-13 17:30:39 +0100

    first draft greetUser function

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-// import date from date
-//
+username = 'john';
 
 
 // greet user function
-// `get hour and assign it to a variable
-// `if hour is < 12  AND  < 18then change the greeting-text to Good Morning, John`
+function greetUser(username) {
+    // `get hour and assign it to a variable
+    let date = new Date();
+    let hour = date.getHours();
+    // `if hour is < 12  AND  < 18then change the greeting-text to Good Morning, John`
+    if (hour < 12 && hour < 18){
+        user_greet = document.querySelector('#greeting-text');
+        user_greet.innerHTML = "Good Morning," + username;
+    }
 // `if hour is > 12  AND < 18 then change greeting-text to Good Afternoon, John`
+    if (hour > 12 && hour < 18){
+        user_greet = document.querySelector('#greeting-text');
+        user_greet.innerHTML = "Good Afternoon," + username;
+    }
 // `if hour is > 12  AND > 18 then change greeting-text to Good Evening, John`
+    if (hour > 12 && hour > 18){
+        user_greet = document.querySelector('#greeting-text');
+        user_greet.innerHTML = "Good Evening," + username;
+    }
+}
 
-// function greetUser(){
-//    
-// }
+
+
+
